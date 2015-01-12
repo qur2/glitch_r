ee1c144bc2a3a86d00c6fc259278304bed8b0e82
Aurélien Scoubeau <aurelien.scoubeau@gmail.com>
2015-01-12 19:59:45 +0100

    Update gulpfile to support multiple browserify packages.

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,65 @@
-var gulp = require('gulp');
+var args = require('yargs').argv;
 var browserify = require('browserify');
-var transform = require('vinyl-transform');
-var uglify = require('gulp-uglify');
+var buffer = require('vinyl-buffer');
+var config = require('./config').browserify;
+var duration = require('gulp-duration');
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var source = require('vinyl-source-stream');
+var sourcemaps = require('gulp-sourcemaps');
+var watchify = require('watchify');
 
 
-gulp.task('browserify', function () {
-  var browserified = transform(function(filename) {
-    var b = browserify(filename);
-    return b.bundle();
-  });
+var isWatching = !!args.watch;
 
-  return gulp.src(['./src/app.js', './src/utils/pixel_compute.js'])
-    .pipe(browserified)
-    // .pipe(uglify())
-    .pipe(gulp.dest('./dist/js'));
-});
+/**
+ * Inspiration taken from:
+ * http://stefanimhoff.de/2014/gulp-tutorial-5-javascripts-browserify/
+ * https://github.com/gulpjs/gulp/blob/master/docs/recipes/fast-browserify-builds-with-watchify.md
+ */
+gulp.task('browserify', function(callback) {
+  // browsersync.notify('Compiling JavaScript');
 
-gulp.task('watch', function () {
-   gulp.watch('src/**/*.js', ['browserify']);
+  var bundleQueue = config.bundleConfigs.length;
+
+  var buildBundle = function(bundleConfig) {
+    var bundler = browserify(bundleConfig.entries, watchify.args);
+
+    var bundle = function() {
+      var timer = duration(bundleConfig.outputName);
+
+      return bundler.bundle()
+        // log errors if they happen
+        .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+        // Use vinyl-source-stream to make the
+        // stream gulp compatible. Specifiy the
+        // desired output filename here.
+        .pipe(source(bundleConfig.outputName))
+        .pipe(timer)
+        // Specify the output destination
+        .pipe(gulp.dest(bundleConfig.dest))
+        .on('end', reportFinished);
+    };
+
+    if (isWatching) {
+      // Wrap with watchify and rebundle on changes
+      bundler = watchify(bundler);
+      // Rebundle on update
+      bundler.on('update', bundle);
+    }
+    bundler.transform('es6ify');
+
+    var reportFinished = function() {
+      if (!--bundleQueue) {
+        // If queue is empty, tell gulp the task is complete.
+        // https://github.com/gulpjs/gulp/blob/master/docs/API.md#accept-a-callback
+        callback();
+      }
+    };
+
+    return bundle();
+  };
+
+  // Start bundling with Browserify for each bundleConfig specified
+  config.bundleConfigs.forEach(buildBundle);
 });
